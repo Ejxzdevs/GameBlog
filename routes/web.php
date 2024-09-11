e34f86e808838f1c88309ec7fbde065d7cd640d3
Ejxzdevs <ejhayyygofredo08@gmail.com>
2024-09-11 16:41:38 +0800

    added middleware for end user routes

--- routes/web.php
+++ routes/web.php
@@ -2,49 +2,36 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
-use App\Livewire\Pages\ViewUserPost;
 
-
+// Landing Page
 Route::get('/', function () {
     return view('livewire.pages.home');
 });
-
-// Client Side
+// Webpage available for non authicated End User
 Route::get('/home', function () {
     return view('livewire.pages.home');
-})->name('home'); // Naming the home route
-
+})->name('home'); // Home Page
 Route::get('/about', function () {
     return view('livewire.pages.about');
-})->name('about'); // Naming the about route
-
+})->name('about'); // About Page
 Route::get('/blog', function () {
     return view('livewire.pages.blog');
-})->name('blog'); // Naming the profile route
-
-// Client Side of End user
-Route::get('/End-user', function () {
-    return view('components.layouts.end-user');
-});
-
-Route::get('/End-user-home', function () {
-    return view('livewire.pages.end-user-home');
-})->name('end-user-home');
-
-Route::get('/post', function () {
-    return view('livewire.pages.write-post');
-})->name('post');
-
-Route::get('/profile', function () {
-    return view('livewire.pages.profile');
-})->name('profile');
-
-// Route::get('/view/{postId}',ViewUserPost::class)->name('view');
-
+})->name('blog'); // Blogs Post
 Route::get('/view/{postId}',function ($postId) {
     return view('livewire.pages.view-user-post', ['postId' => $postId]);
 })->name('view');
 
+// authicated End User
+Route::middleware('is_user')->group(function () {
+    Route::get('/post', function () {
+        return view('livewire.pages.write-post');
+    })->name('post'); // Page for Add post for End user
+
+    Route::get('/profile', function () {
+        return view('livewire.pages.profile');
+    })->name('profile'); // Profile for End user
+});
+// LOG OUT
 Route::get('/logout', function () {
     Auth::logout();
     Session::flush();
