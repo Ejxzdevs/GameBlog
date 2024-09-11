86d0425d8e3da3943d16798d78091faf8ef63066
Ejhay <ejhayyygofredo@gmail.com>
2024-09-11 13:01:45 +0800

    added this new modal for footer quick access to sign in

---
+++ app/Livewire/Modal/FooterSignin.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Livewire\Modal;
+use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+
+class FooterSignin extends Component
+{
+    public $email,$password,$showModal = false;
+
+    public function UserEntry()
+{
+  
+    $validatedData = $this->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+    ]);
+
+    if (Auth::attempt($validatedData )) {
+
+        // // Store user type in the session
+        Session::put('user_type', Auth::user()->user_type);
+        Session::put('user_id', Auth::id());
+        Session::put('user_email', Auth::user()->email);
+        return redirect()->route('home');
+    } else {
+        return redirect()->back();
+    }
+
+}
+    public function openLoginModal()
+    {
+        $this->showModal = true;
+    }
+
+    public function closeLoginModal()
+    {
+        $this->showModal = false;
+    }
+
+    public function render()
+    {
+        return view('livewire.modal.footer-signin');
+    }
+}
