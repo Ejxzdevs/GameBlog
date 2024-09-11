d6460374231aea659c2d7c2110174dab5afbcfd9
Ejhay <ejhayyygofredo@gmail.com>
2024-09-11 13:14:44 +0800

    added this new modal for footer quick access to sign up

---
+++ app/Livewire/Modal/FooterSignup.php
@@ -0,0 +1,47 @@
+<?php
+namespace App\Livewire\Modal;
+use App\Models\User;
+use Livewire\Component;
+use Illuminate\Support\Facades\Hash;
+
+class FooterSignup extends Component
+{
+    public $email;
+    public $password;
+    public $repassword;
+    public $successMessage = "Successfully Create Account";
+    
+
+    public function createUser()
+    {
+        $this->validate([
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:5',
+            'repassword' => 'required|same:password',
+        ]);
+    
+        User::create([
+            'email' => $this->email,
+            'password' => Hash::make($this->password),
+            'user_type' => 'End_User',
+        ]);
+    
+        $this->reset(['email', 'password', 'repassword']);
+        $this->dispatch('alert-success', ['success' => $this->successMessage]);
+    }
+    public $showModal = false;
+    
+    public function openSignupModal()
+    {
+        $this->showModal = true;
+    }
+
+    public function closeSignupModal()
+    {
+        $this->showModal = false;
+    }
+    public function render()
+    {
+        return view('livewire.modal.footer-signup');
+    }
+}
