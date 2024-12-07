2d0105f4aa9950cf642bdcbf9142052a35e8d2af
Ejxzdevs <ejhayyygofredo08@gmail.com>
2024-12-08 02:42:03 +0800

    remove padding

--- resources/views/livewire/pages/profile.blade.php
+++ resources/views/livewire/pages/profile.blade.php
@@ -3,19 +3,15 @@
 @php
     $email = Session::get('user_email');
 @endphp
-<div class="h-100p-0 m-0 ">
-    <div class="px-5 py-2 " style="height: 200px ">
-        <div class="h-100 d-flex flex-row gap-3 primary-color" style="border-radius:5px; box-shadow:1px 1px 2px;">
-            <div class="h-100 d-flex justify-content-end align-items-center" style="width:170px"> 
-                <img src="{{ asset('storage/images/default.png') }}" style="height: 100px" >
-            </div>
-            <div class="h-100 d-flex justify-content-center align-items-center pb-3">
-                <h3 id="username">{{$username = explode('@', $email)[0]}}</h3>
-            </div>
+<div class="h-100 p-0 m-0 ">
+    <div class="d-flex align-items-center justify-content-center gap-2 fm-lato fw-bold border mb-3 rounded" 
+    style="height: 150px; background-image: url('{{ asset('storage/images/games.jpg') }}'); background-size: cover; background-position: center;">
+                <p class="fs-2" style="background: linear-gradient(45deg, #ffffff, #FF6F91, #c6dd17); 
+                -webkit-background-clip: text; background-clip: text; color: transparent;">
+                Check Out Your Profile 
+            </p>
         </div>
-    </div>
-        <div class="h-auto px-5 py-2" >
-            <div class="p-5 primary-color" style="border-radius:5px; box-shadow:1px 1px 2px;">
+            <div>
                 @livewire('partials.components.user-wall')
             </div>
         </div>
