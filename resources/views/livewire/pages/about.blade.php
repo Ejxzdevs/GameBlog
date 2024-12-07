fd55e656a819b562ac46442f14a03bc71bd3725d
Ejxzdevs <ejhayyygofredo08@gmail.com>
2024-12-08 02:41:30 +0800

    center container

--- resources/views/livewire/pages/about.blade.php
+++ resources/views/livewire/pages/about.blade.php
@@ -1,7 +1,7 @@
 @extends('components.layouts.app')
 @section('pages')
 <div class="d-flex flex-column h-100 p-0 m-0">
-    <div class="h-100 p-0 m-0">
+    <div class="h-100 d-flex justify-content-center align-items-center p-0 m-0">
         @livewire('partials.components.system-description')
     </div>
     <div>
