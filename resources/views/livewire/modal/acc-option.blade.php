c33e099ebf500f869c93ffcf8c5f50e458d56c73
Ejxzdevs <ejhayyygofredo08@gmail.com>
2024-12-08 03:19:08 +0800

    change bg

--- resources/views/livewire/modal/acc-option.blade.php
+++ resources/views/livewire/modal/acc-option.blade.php
@@ -1,10 +1,10 @@
-<div style="z-index:1;"> 
+<div style="z-index:1;"  > 
     <a class="nav-icon" wire:click="booleanProfile">
         <i class="bi bi-person-circle {{ $focus ? 'text-info ' : 'text-white' }};"></i>
     </a>
-    <ul class="dropdown-modal" style="display: {{ $Profile ? 'block' : 'none' }};">
-        <li><i class="bi bi-person-circle fs-6"></i> <a class="dropdown-item fw-normal" href="{{ route('profile') }}">Profile</a></li>
-        <li><i class="bi bi-box-arrow-right fs-6"></i><a class="dropdown-item fw-normal" href="{{ route('logout') }}">Log out</a></li>
+    <ul class="dropdown-modal" style="background-color:#181047; display: {{ $Profile ? 'block' : 'none' }}; ">
+        <li style="background-color:#101218;" ><i class="text-white bi bi-person-circle fs-6"></i> <a class="dropdown-item fw-normal text-white" href="{{ route('profile') }}">Profile</a></li>
+        <li style="background-color:#101218;" ><i class="text-white bi bi-box-arrow-right fs-6"></i><a class="dropdown-item fw-normal text-white" href="{{ route('logout') }}">Log out</a></li>
     </ul>
 </div>
 
--- resources/views/livewire/modal/notification.blade.php
+++ resources/views/livewire/modal/notification.blade.php
@@ -2,9 +2,9 @@
     <a class="nav-icon" wire:click="booleanNotification">
         <i class="bi bi-bell {{ $focus ? 'text-info ' : 'text-white' }}; "></i>
     </a>
-    <ul class="dropdown-modal" style="display: {{ $Notification ? 'block'  : 'none' }};">
-        <li><a class="dropdown-item fw-normal" href="{{ route('logout') }}">1</a></li>
-        <li><a class="dropdown-item fw-normal" href="{{ route('logout') }}">2</a></li>
+    <ul class="dropdown-modal" style="background-color:#181047; display: {{ $Notification ? 'block'  : 'none' }};">
+        <li style="background-color:#101218;" ><a class="dropdown-item fw-normal text-white" href="{{ route('logout') }}">1</a></li>
+        <li style="background-color:#101218;" ><a class="dropdown-item fw-normal text-white" href="{{ route('logout') }}">2</a></li>
     </ul>
 </div>
 
