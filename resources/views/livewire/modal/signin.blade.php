77819c261d851413fa9b4542431e5ceb65b1bfbf
Ejxzdevs <ejhayyygofredo08@gmail.com>
2024-12-08 04:13:38 +0800

    adjust fs change bg

--- resources/views/livewire/modal/signin.blade.php
+++ resources/views/livewire/modal/signin.blade.php
@@ -3,36 +3,36 @@
         <i class="bi bi-box-arrow-in-right"></i>
     </a>
 <!-- Modal -->
-<div class="modal fade @if($showModal) show @endif " tabindex="-1" role="dialog " style="display: @if($showModal) block @else none @endif ; background-color: rgba(0, 0, 0, 0.5)">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content primary-color" style="border:none; border-radius: 0;">
+<div class="modal fade @if($showModal) show @endif " tabindex="-1" role="dialog " style="display: @if($showModal) block @else none @endif ; background-color: rgba(0, 0, 0, 0.7); ">
+    <div class="modal-dialog" role="document"  >
+        <div class="modal-content rounded" style="border:none; background-color: #181047; ">
           <div class="modal-header" style="border: none">
-                <button style="border:none" type="button" class="modal-close-btn primary-color" wire:click="closeLoginModal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
+                <button style="border:none; background-color: #181047;" type="button" class="modal-close-btn" wire:click="closeLoginModal" aria-label="Close">
+                    <i  class="text-white bi bi-x-lg"></i>
                 </button>
           </div>
-            <div class="modal-body">
+            <div class="modal-body" >
               <div class="h-100 d-flex justify-content-center align-items-center">
                 <form wire:submit.prevent="UserEntry" class="signup-custom-form d-flex flex-column gap-4" autocomplete="off">
                     <div class="d-flex flex-column justify-content-center align-items-center pt-2 " style="height:15%;">
-                        <h4>User Entry</h4>
+                        <h1 class="fw-medium text-white fs-3" >User Login</h1>
                     </div>
                     <div class="d-flex flex-column justify-content-center ps-5  " style="height: 20%;">
-                        <label class="form-label fw-medium fs-sx">Email</label>
+                        <label class="form-label fw-medium fs-6 text-white">Email</label>
                         <input wire:model="email" type="email" class="custom-width form-control" placeholder="Enter your email" >
                         @error('email')
                           <span class="text-danger fs-6">{{ $message }}</span>
                         @enderror
                     </div>
                     <div class="d-flex flex-column justify-content-center ps-5  " style="height: 20%;">
-                        <label class="form-label fw-medium fs-sx">Password</label>
+                        <label class="form-label fw-medium fs-6 text-white">Password</label>
                         <input wire:model="password" type="password" class="custom-width form-control " placeholder="Enter your password" >
                         @error('password')
                           <span class="text-danger fs-6 mb-2">{{ $message }}</span>
                         @enderror
                     </div>
-                    <div class="d-flex flex-column justify-content-center ps-5" style="height: 25%;">
-                        <button type="submit" class="custom-width btn btn-dark letter-spacing-lg text-capitalize">Submit</button>
+                    <div class="d-flex flex-column justify-content-start pt-3 ps-5" style="height: 25%;">
+                        <button type="submit" class="btn text-white custom-width letter-spacing-lg text-capitalize" style="background-color: #101218;">Submit</button>
                     </div>
                 </form>
             </div>
--- resources/views/livewire/modal/signup.blade.php
+++ resources/views/livewire/modal/signup.blade.php
@@ -11,37 +11,37 @@
   {{-- @endif --}}
 
 
-<div class="modal fade @if($showModal) show @endif " tabindex="-1" role="dialog " style="display: @if($showModal) block @else none @endif ; background-color: rgba(0, 0, 0, 0.5)">
+<div class="modal fade @if($showModal) show @endif " tabindex="-1" role="dialog " style="display: @if($showModal) block @else none @endif ; background-color: rgba(0, 0, 0, 0.7)">
   <div class="modal-dialog" role="document">
-      <div class="modal-content primary-color" style="border:none; border-radius: 0;">
+      <div class="modal-content rounded" style="border:none; border-radius: 0; background-color: #181047;">
         <div class="modal-header" style="border: none">
-              <button style="border:none" type="button" class="modal-close-btn primary-color" wire:click="closeSignupModal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
+              <button style="border:none;    background-color: #181047;" type="button" class="modal-close-btn primary-color" wire:click="closeSignupModal" aria-label="Close">
+                  <i class="text-white bi bi-x-lg"></i>
               </button>
         </div>
           <div class="modal-body">
             <div class="h-100 d-flex justify-content-center align-items-center">
               <form wire:submit.prevent="createUser" class="signup-custom-form d-flex flex-column gap-4" autocomplete="off">
                   <div class="d-flex flex-column justify-content-center align-items-center pt-2 " style="height:15%;">
-                      <h4>Join Now!</h4>
+                      <h4 class="fw-medium text-white fs-3">Join Now!</h4>
                   </div>
                   <div class="d-flex flex-column justify-content-center ps-5  " style="height: 20%;">
-                      <label class="form-label fw-medium fs-sx">Email</label>
-                      <input wire:model="email" type="email" class="custom-width form-control" placeholder="Enter your email" >
+                      <label class="form-label fw-medium fs-6 text-white">Email</label>
+                      <input wire:model="email" type="email" class="custom-width form-control" placeholder="Enter Email:" >
                       @error('email')
                         <span class="text-danger fs-6">{{ $message }}</span>
                       @enderror
                   </div>
                   <div class="d-flex flex-column justify-content-center ps-5  " style="height: 20%;">
-                      <label class="form-label fw-medium fs-sx">Password</label>
-                      <input wire:model="password" type="password" class="custom-width form-control " placeholder="Enter your password" >
+                      <label class="form-label fw-medium fs-6 text-white">Password</label>
+                      <input wire:model="password" type="password" class="custom-width form-control " placeholder="Enter Password:" >
                       @error('password')
                         <span class="text-danger fs-6 mb-2">{{ $message }}</span>
                       @enderror
                   </div>
                   <div class="d-flex flex-column justify-content-center ps-5  " style="height: 20%;">
-                      <label class="form-label fw-medium fs-sx">Confirm Password</label>
-                      <input wire:model="repassword" type="password" class="custom-width form-control" placeholder="Re-Enter your password">
+                      <label class="form-label fw-medium fs-6 text-white">Confirm Password</label>
+                      <input wire:model="repassword" type="password" class="custom-width form-control" placeholder="Re-Enter Password:">
                       @error('repassword')
                         <span class="text-danger fs-6">{{ $message }}</span>
                       @enderror
@@ -49,8 +49,8 @@
                   
 
                   </div>
-                  <div class="d-flex flex-column justify-content-center ps-5" style="height: 25%;">
-                      <button type="submit" class="custom-width btn btn-dark letter-spacing-lg text-capitalize">Submit</button>
+                  <div class="d-flex flex-column justify-content-start pt-3 ps-5" style="height: 25%;">
+                      <button style="background-color: #101218;" type="submit" class="custom-width text-white btn letter-spacing-lg text-capitalize">Submit</button>
                   </div>
               </form>
           </div>
