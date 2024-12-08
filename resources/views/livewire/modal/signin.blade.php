79be27375fa7a1276471b6ffc71f2d222908d327
Ejxzdevs <ejhayyygofredo08@gmail.com>
2024-12-08 19:30:05 +0800

    adjust label change font family

--- resources/views/livewire/modal/signin.blade.php
+++ resources/views/livewire/modal/signin.blade.php
@@ -7,32 +7,33 @@
     <div class="modal-dialog pt-2" role="document"  >
         <div class="modal-content rounded mt-5" style="border:none; background: rgba(255, 255, 255, 0.1); backdrop-filter: blur(10px);  ">
           <div class="modal-header" style="border: none">
-                <button style="border:none; background-color: #181047;" type="button" class="modal-close-btn rounded" wire:click="closeLoginModal" aria-label="Close">
+                <button style="border:none; background-color: #101218;" type="button" class="modal-close-btn rounded" wire:click="closeLoginModal" aria-label="Close">
                     <i  class="text-white bi bi-x-lg"></i>
                 </button>
           </div>
             <div class="modal-body" >
               <div class="h-100 d-flex justify-content-center align-items-center">
-                <form wire:submit.prevent="UserEntry" class="signup-custom-form d-flex flex-column gap-4" autocomplete="off">
+                <form wire:submit.prevent="UserEntry" class="signup-custom-form d-flex flex-column gap-2" autocomplete="off">
                     <div class="d-flex flex-column justify-content-center align-items-center pt-2 " style="height:15%;">
-                        <h1 class="fw-medium text-white fs-3 orbitron" >Welcome To GameBlog</h1>
+                        <h1 class="fw-bold fs-2 orbitron" style="background: linear-gradient(45deg, #ffffff, #FF6F91, #9d90ea); 
+                -webkit-background-clip: text; background-clip: text; color: transparent" >GameBlog Sign In</h1>
                     </div>
                     <div class="d-flex flex-column justify-content-center ps-5  " style="height: 20%;">
-                        <label class="form-label fw-medium fs-6 text-white">Email</label>
-                        <input wire:model="email" type="email" class="custom-width form-control" placeholder="Enter your email" >
+                        <label class="form-label fw-medium fs-6 text-white nunito p-0 m-0">Email</label>
+                        <input style="font-size: 14px;"  wire:model="email" type="email" class="custom-width form-control orbitron" placeholder="Enter Email:" >
                         @error('email')
                           <span class="text-danger fs-6">{{ $message }}</span>
                         @enderror
                     </div>
                     <div class="d-flex flex-column justify-content-center ps-5  " style="height: 20%;">
-                        <label class="form-label fw-medium fs-6 text-white">Password</label>
-                        <input wire:model="password" type="password" class="custom-width form-control " placeholder="Enter your password" >
+                        <label class="form-label fw-medium fs-6 text-white nunito p-0 m-0">Password</label>
+                        <input style="font-size: 14px;" wire:model="password" type="password" class="custom-width form-control orbitron" placeholder="Enter Password: " >
                         @error('password')
                           <span class="text-danger fs-6 mb-2">{{ $message }}</span>
                         @enderror
                     </div>
                     <div class="d-flex flex-column justify-content-start pt-3 ps-5" style="height: 25%;">
-                        <button type="submit" class="btn text-white custom-width letter-spacing-lg text-capitalize" style="background-color: #101218;">Submit</button>
+                        <button type="submit" class="btn text-white custom-width letter-spacing-lg nunito fw-bold" style="background-color: #101218;">SIGN IN</button>
                     </div>
                 </form>
             </div>
--- resources/views/livewire/modal/signup.blade.php
+++ resources/views/livewire/modal/signup.blade.php
@@ -15,7 +15,7 @@
   <div class="modal-dialog pt-3" role="document">
       <div class="modal-content rounded mt-5" style="border:none; border-radius: 0; background: rgba(255, 255, 255, 0.1); backdrop-filter: blur(10px);  ">
         <div class="modal-header" style="border: none">
-              <button style="border:none;    background-color: #181047;" type="button" class="modal-close-btn primary-color rounded" wire:click="closeSignupModal" aria-label="Close">
+              <button style="border:none;    background-color: #101218;" type="button" class="modal-close-btn primary-color rounded" wire:click="closeSignupModal" aria-label="Close">
                   <i class="text-white bi bi-x-lg"></i>
               </button>
         </div>
@@ -23,25 +23,26 @@
             <div class="h-100 d-flex justify-content-center align-items-center">
               <form wire:submit.prevent="createUser" class="signup-custom-form d-flex flex-column gap-4" autocomplete="off">
                   <div class="d-flex flex-column justify-content-center align-items-center pt-2 " style="height:15%;">
-                      <h4 class="fw-medium text-white fs-3 orbitron">Join GameBlog</h4>
+                      <h4 style="background: linear-gradient(45deg, #ffffff, #FF6F91, #9d90ea); 
+                -webkit-background-clip: text; background-clip: text; color: transparent" class="fw-medium fs-3 orbitron">GameBlog Sign Up</h4>
                   </div>
                   <div class="d-flex flex-column justify-content-center ps-5  " style="height: 20%;">
-                      <label class="form-label fw-medium fs-6 text-white">Email</label>
-                      <input wire:model="email" type="email" class="custom-width form-control" placeholder="Enter Email:" >
+                      <label class="form-label fw-medium fs-6 text-white nunito p-0 m-0">Email</label>
+                      <input style="font-size: 14px;" wire:model="email" type="email" class="custom-width form-control orbitron" placeholder="Enter Email:" >
                       @error('email')
                         <span class="text-danger fs-6">{{ $message }}</span>
                       @enderror
                   </div>
                   <div class="d-flex flex-column justify-content-center ps-5  " style="height: 20%;">
-                      <label class="form-label fw-medium fs-6 text-white">Password</label>
-                      <input wire:model="password" type="password" class="custom-width form-control " placeholder="Enter Password:" >
+                      <label class="form-label fw-medium fs-6 text-white nunito p-0 m-0">Password</label>
+                      <input style="font-size: 14px;" wire:model="password" type="password" class="custom-width form-control orbitron " placeholder="Enter Password:" >
                       @error('password')
                         <span class="text-danger fs-6 mb-2">{{ $message }}</span>
                       @enderror
                   </div>
                   <div class="d-flex flex-column justify-content-center ps-5  " style="height: 20%;">
-                      <label class="form-label fw-medium fs-6 text-white">Confirm Password</label>
-                      <input wire:model="repassword" type="password" class="custom-width form-control" placeholder="Re-Enter Password:">
+                      <label class="form-label fw-medium fs-6 text-white nunito p-0 m-0">Confirm Password</label>
+                      <input style="font-size: 14px;" wire:model="repassword" type="password" class="custom-width form-control orbitron" placeholder="Re-Enter Password:">
                       @error('repassword')
                         <span class="text-danger fs-6">{{ $message }}</span>
                       @enderror
@@ -50,7 +51,7 @@
 
                   </div>
                   <div class="d-flex flex-column justify-content-start pt-3 ps-5" style="height: 25%;">
-                      <button style="background-color: #101218;" type="submit" class="custom-width text-white btn letter-spacing-lg text-capitalize">Submit</button>
+                      <button style="background-color: #101218;" type="submit" class="custom-width text-white btn letter-spacing-lg nunito fw-bold">SIGN UP</button>
                   </div>
               </form>
           </div>
