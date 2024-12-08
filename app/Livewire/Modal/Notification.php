b5d52c13bd6b13cecdae3e41748b2fffc6c7217f
Ejxzdevs <ejhayyygofredo08@gmail.com>
2024-12-08 17:37:59 +0800

    style modal notidications

--- app/Livewire/Modal/Notification.php
+++ app/Livewire/Modal/Notification.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Livewire\Modal;
+use App\Models\Post;
+use Illuminate\Support\Facades\Session;
 
 use Livewire\Component;
 
@@ -15,16 +17,19 @@ class Notification extends Component
         $this->Notification = false;
    
     }
-
     public function booleanNotification() {
         
         $this->Notification = !$this->Notification;
         $this->focus = !$this->focus;
         
     }
-
     public function render()
     {
-        return view('livewire.modal.notification');
+        $data = Post::query()
+        ->join('likes', 'posts.id', '=', 'likes.post_id')
+        ->join('users', 'likes.user_id', '=', 'users.id')
+        ->where('posts.user_id', Session::get('user_id'))
+        ->get();
+        return view('livewire.modal.notification',['data' => $data]);
     }
 }
