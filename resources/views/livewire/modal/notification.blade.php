d61d99e98d74b8f7dbab81f8f223b4fd46106c2c
Ejxzdevs <ejhayyygofredo08@gmail.com>
2024-12-08 17:37:43 +0800

    add container for noti

--- resources/views/livewire/modal/notification.blade.php
+++ resources/views/livewire/modal/notification.blade.php
@@ -2,9 +2,16 @@
     <a class="nav-icon" wire:click="booleanNotification">
         <i class="bi bi-bell {{ $focus ? 'text-info ' : 'text-white' }}; "></i>
     </a>
-    <ul class="dropdown-modal" style="background-color:#181047; display: {{ $Notification ? 'block'  : 'none' }};">
-        <li style="background-color:#101218;" ><a class="dropdown-item fw-normal text-white" href="{{ route('logout') }}">1</a></li>
-        <li style="background-color:#101218;" ><a class="dropdown-item fw-normal text-white" href="{{ route('logout') }}">2</a></li>
-    </ul>
+    <div class="notification overflow-y-auto py-2 px-2 " style="background-color:#181047; display: {{ $Notification ? 'block'  : 'none' }} ;">
+        @foreach ($data as $post)
+        <div class="d-flex justify-content-center align-items-center rounded border py-2 px-4 mt-2" style="background-color: #101218 ;" >
+            <p class="text-white fw-lighter" >
+                <span class="fw-medium" >{{$post->user->email}}</span> 
+                <span class="nunito" style="font-size: 15px" >likes your</span>
+                <span href="{{ route('view', ['postId' => $post->post_id]) }}" wire:navigate class="text-primary" style="font-size: 15px; cursor: pointer;" >{{$post->title}}</span></p>
+        </div>
+        @endforeach
+    </div>
+  
 </div>
 
