2aab6be5390a2cd425713b0056fa8fc1fff7aa4d
Ejxzdevs <ejhayyygofredo08@gmail.com>
2024-12-09 00:47:01 +0800

    add label notifications

--- resources/views/livewire/modal/notification.blade.php
+++ resources/views/livewire/modal/notification.blade.php
@@ -3,12 +3,13 @@
         <i class="bi bi-bell {{ $focus ? 'text-info ' : 'text-white' }}; "></i>
     </a>
     <div class="notification overflow-y-auto py-2 px-2 " style="background-color:#181047; display: {{ $Notification ? 'block'  : 'none' }} ;  scrollbar-width: thin;">
+        <p class="fs-6 p-0 m-0 ms-1 text-white nunito fw-bold" >Notifications</p>
         @foreach ($data as $post)
-        <div class="d-flex justify-content-center align-items-center rounded border py-2 px-4 mt-2" style="background-color: #101218 ;" >
+        <div class="d-flex justify-content-center align-items-center rounded border py-1 px-4 mt-2" style="background-color: #101218 ;" >
             <p class="text-white fw-lighter" >
-                <span class="fw-medium" >{{$post->user->email}}</span> 
-                <span class="nunito" style="font-size: 15px" >likes your</span>
-                <span href="{{ route('view', ['postId' => $post->post_id]) }}" wire:navigate class="text-primary" style="font-size: 15px; cursor: pointer;" >{{$post->title}}</span></p>
+                <span class="fw-medium" style="font-size: 14px" >{{$post->user->email}}</span> 
+                <span class="nunito" style="font-size: 13px" >likes your</span>
+                <span href="{{ route('view', ['postId' => $post->post_id]) }}" wire:navigate class="text-primary" style="font-size: 13px; cursor: pointer;" >{{$post->title}}</span></p>
         </div>
         @endforeach
     </div>
